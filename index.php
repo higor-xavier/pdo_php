d8fd136d5aa8779bb333ebeb6c7c51fe14cc56b0
higor-xavier <higorsxavier@gmail.com>
2023-04-21 20:22:35 -0300

    Different types of results

--- index.php
+++ index.php
@@ -11,7 +11,12 @@
 		$query = "SELECT * FROM tb_usuarios";
 
 		$stmt = $con->query($query);
-		$lista = $stmt->fetchAll();
+
+		//FETCH_ASSOC = nome associado a coluna no retorno
+		//FETCH_NUM = indice associado a tupla no retorno
+		//FETCH_BOTH = os dois acima
+		//FETCH_OBJ = torna o retorno em um objeto
+		$lista = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 		echo "<pre>";
 		print_r($lista);
