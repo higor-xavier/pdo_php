4466a51b5bb3ab7d321e6ff0cf8874d0b37dbe73
higor-xavier <higorsxavier@gmail.com>
2023-04-21 20:38:08 -0300

    Using foreach to iterate statements results

--- index.php
+++ index.php
@@ -8,22 +8,29 @@
 	try {
 		$con = new PDO($dsn, $user, $password);
 
-		$query = "SELECT * FROM tb_usuarios WHERE id=2";
+		$query = "SELECT * FROM tb_usuarios";
 
-		$stmt = $con->query($query);
+		//$stmt = $con->query($query);
+
+		foreach ($con->query($query) as $key => $value) {
+			print_r($value['nome']);
+			echo "<hr>";
+		}
 
 		//FETCH_ASSOC = nome associado a coluna no retorno
 		//FETCH_NUM = indice associado a tupla no retorno
 		//FETCH_BOTH = os dois acima
 		//FETCH_OBJ = torna o retorno em um objeto
-		$usuario = $stmt->fetch(PDO::FETCH_OBJ);
+		//$lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-		echo "<pre>";
-		print_r($usuario);
-		echo "</pre>";
+		// echo "<pre>";
+		// print_r($lista_usuarios);
+		// echo "</pre>";
 
-		echo $usuario->nome;
-
+		// foreach ($lista_usuarios as $key => $value) {
+		// 	echo $value['nome'];
+		// 	echo "<hr>";
+		// }
 
 	} catch (PDOException $e) {
 		echo 'Erro: '.$e->getCode(). ' Mensagem: '.$e->getMessage();
