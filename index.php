27cfaf195efdac36f5c700fc9c3c4c0340fdb2fa
higor-xavier <higorsxavier@gmail.com>
2023-04-21 21:01:52 -0300

    SQL Injection test

--- index.php
+++ index.php
@@ -1,42 +1,54 @@
 <?php 
 
-	//DATA SOURCE NAME
-	$dsn = 'mysql:host=localhost; dbname=php_com_pdo';
-	$user = 'root';
-	$password = '';
+	if (!empty($_POST['usuario']) && !empty($_POST['senha']) ) {
 
-	try {
-		$con = new PDO($dsn, $user, $password);
+		//DATA SOURCE NAME
+		$dsn = 'mysql:host=localhost; dbname=php_com_pdo';
+		$user = 'root';
+		$password = '';
 
-		$query = "SELECT * FROM tb_usuarios";
+		try {
+			$con = new PDO($dsn, $user, $password);
 
-		//$stmt = $con->query($query);
+			$query = "SELECT * FROM tb_usuarios WHERE";
+			$query .= " email = '{$_POST['usuario']}'";
+			$query .= " AND senha = '{$_POST['senha']}'";
 
-		foreach ($con->query($query) as $key => $value) {
-			print_r($value['nome']);
+			echo($query);
 			echo "<hr>";
+
+			$stmt = $con->query($query);
+			$usuario = $stmt->fetch();
+
+			echo "<pre>";
+			print_r($usuario);
+			echo "</pre>";
+
+		} catch (PDOException $e) {
+			echo 'Erro: '.$e->getCode(). ' Mensagem: '.$e->getMessage();
+			//registrar erro
+			
 		}
-
-		//FETCH_ASSOC = nome associado a coluna no retorno
-		//FETCH_NUM = indice associado a tupla no retorno
-		//FETCH_BOTH = os dois acima
-		//FETCH_OBJ = torna o retorno em um objeto
-		//$lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-		// echo "<pre>";
-		// print_r($lista_usuarios);
-		// echo "</pre>";
-
-		// foreach ($lista_usuarios as $key => $value) {
-		// 	echo $value['nome'];
-		// 	echo "<hr>";
-		// }
-
-	} catch (PDOException $e) {
-		echo 'Erro: '.$e->getCode(). ' Mensagem: '.$e->getMessage();
-		//registrar erro
-		
 	}
 
-
  ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<title>Login</title>
+ </head>
+ <body>
+
+ 	<form method="POST" action="index.php">
+ 		<input type="text" name="usuario" placeholder="usuario">
+ 		<br>
+ 		<input type="password" name="senha" placeholder="senha">
+ 		<br>
+ 		<button type="submit">Logar</button>
+ 	</form>
+ 
+ </body>
+ </html>
